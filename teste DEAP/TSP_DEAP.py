870474e620353f511ab3dd111ecd87f5db390fa1
daniel <daniel.csouza@usp.br>
2021-09-06 11:27:55 -0300

    eA simples funcionando para o TSP

--- teste DEAP/TSP_DEAP.py
+++ teste DEAP/TSP_DEAP.py
@@ -1,73 +1,76 @@
 import matplotlib.pyplot as plt
-import sys 
-import array
-import random
-import numpy as np
-from deap import algorithms
-from deap import base
-from deap import creator
-from deap import tools
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+from deap import algorithms, base, creator, tools
+import random, operator, time, itertools, math
+import numpy
 
-#setando cidades
-numCidades=10
-random.seed(169)
-x= np.random.rand(numCidades)
-y= np.random.rand(numCidades)
-'''
-print(x)
-print(y)
-plt.plot(x,y)
-plt.show()
-'''
 
-#p/ minimizar a distancia, pesos devem ser negativos
-creator.create('FitnessMin',base.Fitness,weights=(-1.0,))
-#os individuos serão vetores de inteiros dimensão 1xnCidades)
-creator.create('Individuo',array.array,typecode='i',fitness=creator.FitnessMin)
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rcParams['text.latex.preamble'] ='\\usepackage{libertine}\n\\usepackage[utf8]{inputenc}'
+
+import seaborn
+seaborn.set(style='whitegrid')
+seaborn.set_context('notebook')
+
+City=complex
+def distance(A, B):
+    "The Euclidean distance between two cities."
+    return abs(A - B)
+def total_distance(tour):
+    "The total distance between each pair of consecutive cities in the tour."
+    return sum(distance(tour[i], tour[i-1]) for i in range(len(tour)))
+def generate_cities(n):
+    "Make a set of n cities, each with random coordinates."
+    return set(City(random.randrange(10, 890), random.randrange(10, 590)) for c in range(n))
+
+num_cities = 30
+cities = generate_cities(num_cities)
+
+def plot_tour(tour, alpha=1, color=None):
+    # Plot the tour as blue lines between blue circles, and the starting city as a red square.
+    plotline(list(tour) + [tour[0]], alpha=alpha, color=color)
+    plotline([tour[0]], style='gD', alpha=alpha, size=10)
+    plt.show()
+
+def plotline(points, style='bo-', alpha=1, size=7, color=None):
+    "Plot a list of points (complex numbers) in the 2-D plane."
+    X, Y = XY(points)
+
+    if color:
+        plt.plot(X, Y, style, alpha=alpha, markersize=size, color=color)
+    else:
+        plt.plot(X, Y, style, alpha=alpha, markersize=size)
+
+def XY(points):
+    "Given a list of points, return two lists: X coordinates, and Y coordinates."
+    return [p.real for p in points], [p.imag for p in points]
 
 toolbox=base.Toolbox()
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
-#gerando atributos
-toolbox.register("indices",random.sample,range(numCidades),numCidades)
-#print(random.sample(range(numCidades),numCidades))
+toolbox.register("indices", numpy.random.permutation, len(cities))
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-toolbox.register('individuo',tools.initIterate,creator.Individuo,toolbox.indices)
-toolbox.register('populacao',tools.initRepeat,list,toolbox.individuo)
-toolbox.register('mate',tools.cxOrdered)
-toolbox.register('mutate',tools.mutShuffleIndexes, indpb=0.005)
-toolbox.register('select', tools.selTournament,tournsize=3)
+toolbox.register("mate", tools.cxOrdered)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
 
-def aval(i):
-    print(i)
-    diffx=np.diff(x[i])
-    diffy=np.diff(y[i])
-    distancia= np.sum(diffx**2+diffy**2)
-    return distancia
+def create_tour(individual):
+    return [list(cities)[e] for e in individual]
 
-toolbox.register('evaluate',aval)
+def evaluation(individual):
+    '''Evaluates an individual by converting it into
+    a list of cities and passing that list to total_distance'''
+    return (total_distance(create_tour(individual)),)
 
-def main():
+toolbox.register("evaluate", evaluation)
+toolbox.register("select", tools.selTournament, tournsize=3)
+pop = toolbox.population(n=100)
+result, log = algorithms.eaSimple(pop, toolbox,cxpb=0.8, mutpb=0.2,ngen=400, verbose=False)
+best_individual = tools.selBest(result, k=1)[0]
+print('Fitness of the best individual: ', evaluation(best_individual)[0])
 
-    random.seed(169)
-    pop=toolbox.populacao(n=300)
-    hof=tools.HallOfFame(1)
-
-    stats=tools.Statistics(lambda ind:ind.fitness.values)
-    stats.register('avg',np.mean)
-    stats.register('std',np.std)
-    stats.register('min',np.min)
-    stats.register('max',np.max)
-    
-    algorithms.eaSimple(pop,toolbox,0.7,0.2,140,stats=stats,halloffame=hof)
-
-    ind=hof[0]
-    plt.figure(2)
-    plt.plot(x[ind],y[ind])
-    plt.ion()
-    plt.show()
-    plt.pause(0.001)
-
-    return pop,stats,hof
-
-if __name__ =="__main__":
-    main()
+plot_tour(create_tour(best_individual))
