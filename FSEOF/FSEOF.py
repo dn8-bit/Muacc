f840766ff0d2c29c399b9607befa9d489ef105a1
daniel <daniel.csouza@usp.br>
2021-09-28 15:05:44 -0300

    Primeiros resultados

--- FSEOF/FSEOF.py
+++ FSEOF/FSEOF.py
@@ -24,14 +24,14 @@ cobra_model.objective=biomass_id
 fluxos_iniciais= cobra_model.optimize().fluxes
 biomassa_0= fluxos_iniciais[biomass_id]
 v_0=fluxos_iniciais[alvo]
-fluxos_iniciais.to_csv('Fluxos Iniciais')
+fluxos_iniciais.to_csv('fluxos/Fluxos Iniciais')
 #maximização
 
 cobra_model.objective=alvo
 fluxos_maximo=cobra_model.optimize().fluxes
 biomassa_final=fluxos_maximo[biomass_id]
 v_final=fluxos_maximo[alvo]
-fluxos_maximo.to_csv('Fluxos Maximos')
+fluxos_maximo.to_csv('fluxos/Fluxos Maximos')
 #calculo dos passos
 fluxos_forcados=[]
 n=float(n_vezes)+1
@@ -51,14 +51,14 @@ for item in fluxos_forcados:
     reacao_alvo.upper_bound=item
     reacao_alvo.lower_bound=item
     simulacao= cobra_model.optimize().fluxes
-    simulacao.to_csv(f'Fluxos iteracao {i}')
+    simulacao.to_csv(f'fluxos/Fluxos iteracao {i}')
     lista_fluxos.append(simulacao)
     a=[]
     for reacao in cobra_model.reactions:
         if lista_fluxos[i-1][reacao.id]<lista_fluxos[i][reacao.id] and lista_fluxos[i][reacao.id]*lista_fluxos[i-1][reacao.id]>=0:
             a.append(reacao.id)
     fva=cobra.flux_analysis.flux_variability_analysis(cobra_model,reaction_list=a)
-    fva.to_csv(f'FVA iteracao {i}')
+    fva.to_csv(f'FVA/FVA iteracao {i}')
     lista_fva.append(fva)
     lista_selecionadas.append(a)
 
